8c586de578b291ed8c0f580632c292cd5d6fdeea
ruslan <karabetrus@gmail.com>
2019-07-11 17:17:10 -0500

    added auth folder, moved auth middleware

--- api/server.js
+++ api/server.js
@@ -29,3 +29,6 @@ server.get("/", (req, res) => {
 
 // Server export to be used in index.js
 module.exports = server;
+
+// Login sign-up post
+// middleware checking the token
---
+++ auth/token.js
@@ -0,0 +1,4 @@
+function decode() {
+    const token = req.header.authorization
+    console.log(token)
+  }
