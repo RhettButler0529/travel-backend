342a4345c7c078b4e34a9dcdacde7e9c86716d47
ruslan <karabetrus@gmail.com>
2019-08-07 11:01:46 -0700

    Add pseudo code cache function

--- api/routes/Places/index.js
+++ api/routes/Places/index.js
@@ -11,18 +11,38 @@ const infoData = require('../../../mock/dev/info');
 
 const router = express.Router();
 
-router.get('/details/:city', mock(cityData), async (req, res) => {
+const checkCache = async (req, res, next) => {
+  /*
+
+    Client sends text query
+    Lookup query in alias table
+
+    Does query exist in alias table
+    | Yes => get referenced destination id
+    | No => Make findPlace query to get place_id
+      | Does response types include 'locality'
+        | No => What do? Send Error? Just continue without caching as destination?
+        | Yes => Continue
+      | Does place_id exist in destination table
+        | Yes => Add alias reference to destination
+        | No => Add new entry in destination table and alias reference
+      | Return long/lat array
+  */
+  next();
+};
+
+router.get('/details/:city', mock(cityData), checkCache, async (req, res) => {
   try {
     const { params } = req;
     const { json: { candidates } } = await googleMapsClient.findPlace({
       input: params.city,
       inputtype: 'textquery',
       language: 'en',
-      fields: ['geometry'],
+      fields: ['geometry', 'types', 'name'],
     }).asPromise();
 
     // pulls the longitude/latitude off the first candidate in the response
-    const { geometry: { location } } = candidates[0];
+    const { geometry: { location }, types: cityTypes, name: cityName } = candidates[0];
 
     const { json: { results } } = await googleMapsClient.places({
       query: 'stuff to do',
@@ -60,10 +80,9 @@ router.get('/details/:city', mock(cityData), async (req, res) => {
       };
     }));
 
-    // parse data and cache to db if needed
-
     res.send({
       status: 'success',
+      cityName,
       places: places.sort((a, b) => (b.rating - a.rating)),
     });
   } catch (error) {
@@ -86,7 +105,6 @@ router.get('/info/:attraction', mock(infoData), async (req, res) => {
       },
     });
     const { result: { detailedDescription: { articleBody: description } } } = itemListElement[0];
-    // console.log(info); // eslint-disable-line
     res.json({
       status: 'success',
       description,
