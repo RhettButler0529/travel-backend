a4a6ae38f38e05c36356b3b6ee905137ae35eebc
ruslan <karabetrus@gmail.com>
2019-08-19 16:48:14 -0700

    Start adding favorite mutation

--- api/graphqlServer.js
+++ api/graphqlServer.js
@@ -3,14 +3,23 @@ const graphql = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const userDb = require('../mock/users')(30);
+const UserFavorite = require('../database/models')('user_favorite');
 const authorize = require('./auth/authorize');
 
 // GraphQL buildSchema
 const schema = buildSchema(`
     type Query  {
-        message: String,
+        message: Message,
         user(id: String!): User,
         users: [User]
+    },
+    type Mutation {
+      addFavorite: Favorite,
+    },
+    type Favorite {
+      id: Int,
+      user_id: String,
+      attraction_id: Int,
     },
     type User {
       id: String,
@@ -34,23 +43,36 @@ const schema = buildSchema(`
       price: Int,
       rating: Float,
       numRatings: Int,
+    },
+    type Message {
+      message: String,
+      user: Test,
+    },
+    type Test {
+      id: String,
+      name: String,
+      email: String,
     }
 `);
 
 // resolvers
-const root = {
-  message: () => 'Hello World',
+const root = req => ({
+  message: () => ({
+    message: 'Hello World',
+    user: req.user,
+  }),
   user: ({ id }) => userDb.find(user => user.id === id),
   users: () => userDb,
-};
+  addFavorite: async () => UserFavorite.add({ user_id: req.user.id, attraction_id: 1 }),
+});
 
-// Create an express server and GraphQL endpoint
+// Create an express router and GraphQL endpoint
 const app = express.Router();
 
-app.use('/', authorize, graphql({
+app.use('/', authorize, graphql(async (req, res, gqlParams) => ({
   schema,
-  rootValue: root,
+  rootValue: await root(req),
   graphiql: true,
-}));
+})));
 
 module.exports = app;
