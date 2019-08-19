b73c55d44e57287ecbe6b7e5876ec03d7dc1bfcc
ruslan <karabetrus@gmail.com>
2019-08-19 13:05:26 -0700

    Refactor token to use async/await

--- api/auth/token.js
+++ api/auth/token.js
@@ -1,34 +1,61 @@
 const axios = require('axios');
 require('dotenv').config();
 
-module.exports = (req, res, next) => {
-  axios
-    .get(
-      `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${req.headers.authorization}`,
-    )
-    .then(({ data }) => {
-      const {
-        sub: id, aud, email, given_name: first, family_name: last,
-      } = data;
-
-      // validate client id
-      if (aud !== process.env.OAUTH_GOOGLE_ID) {
-        return res.status(401).json({
-          message: 'Invalid Client ID',
-        });
-      }
-
-      req.user = {
-        id,
+module.exports = async (req, res, next) => {
+  try {
+    const {
+      data: {
+        sub: id,
+        aud,
         email,
-        name: `${first} ${last}`,
-        token: req.headers.authorization,
-      };
+        given_name: first,
+        family_name: last,
+      },
+    } = await axios
+      .get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${req.headers.authorization}`);
 
-      return next();
-    })
-    .catch((err) => {
-      // console.log("Error:  ", err.response);
-      res.status(401).json({ message: 'You need to log in first', error: err });
+    // const response = await axios.get(
+    //   `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${req.headers.authorization}`
+    // );
+
+    // validate client id
+    if (aud !== process.env.OAUTH_GOOGLE_ID) {
+      return res.status(401).json({
+        status: 'error',
+        message: 'Invalid Client ID provided',
+      });
+    }
+
+    req.user = {
+      id,
+      email,
+      name: `${first} ${last}`,
+      token: req.headers.authorization,
+    };
+
+    return next();
+  } catch (error) {
+    // catch bad requests
+    if (error.message === 'Request failed with status code 400') {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    // catch bad auth (expired token)
+    if (error.message === 'Request failed with a status code 401') {
+      return res.status(401).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    // default error catch
+    return res.status(500).json({
+      status: 'error',
+      message: 'Unknown Server Error',
+      raw: error,
     });
+  }
 };
