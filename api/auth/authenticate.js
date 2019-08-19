532b71c8a35328a81155d25284d4ee70d6e68a4d
ruslan <karabetrus@gmail.com>
2019-08-19 15:28:08 -0700

    Add login authentication

--- api/auth/authenticate.js
+++ api/auth/authenticate.js
@@ -1,19 +1,45 @@
-/*
-  authenticate: initial token login, update database info
-*/
 const decode = require('./decode');
 const User = require('../resources/User/user.model');
 
 module.exports = async (req, res, next) => {
-  // find user first
-  const user = await User.findById(req.user.id);
+  try {
+    const { authorization: token } = req.headers;
+    const {
+      sub: id,
+      aud: clientId,
+      email,
+      given_name: first,
+      family_name: last,
+    } = await decode.remote(token);
 
-  if (user) {
-    // verify token exists
-    next();
-  } else {
-    // if user doesn't exist, then insert
-    User.insert(req.user);
-    next();
+    if (clientId !== process.env.OAUTH_GOOGLE_ID) {
+      // mismatch client ID, super sketch
+      return res.status(400).json({
+        status: 'error',
+        message: 'Invalid Client ID',
+      });
+    }
+
+    req.user = {
+      id,
+      email,
+      name: `${first} ${last}`,
+      token,
+    };
+
+    const user = await User.get(id);
+
+    if (user) {
+      await User.update(id, { token });
+    } else {
+      await User.add(req.user);
+    }
+
+    return next();
+  } catch (error) {
+    return res.status(500).json({
+      status: 'error',
+      message: 'Unknown Server Error',
+    });
   }
 };
